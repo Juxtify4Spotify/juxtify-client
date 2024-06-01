b1640f52bcaf83457065d948a4c2731f67cb9c08
出口レイ <richard@kanshen.click>
2024-06-01 20:23:34 +0200

    Reworked the Tailwind colors
    Co-Authored-By: kenn <matt@kenn.pro>

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,18 +9,37 @@ export default {
         'satoshi': ['"Satoshi"', 'sans-serif'] 
       },
       colors: {
-        'bg-dark-main': '#060606',
-        'bg-dark-primary': '#111111',
-        'bg-dark-secondary': '#202020',
-
-        'bg-light': '#F0F0F0',
-
-        'fg-dark-primary': '#E0E0E0',
-        'fg-dark-secondary': '#898989',
-        'fg-dark-tertiary': '#323232',
-
-        'fg-light-primary': '#1A1A1A',
-        'fg-light-secondary': '#6E6E6E',
+        'bg-main': {
+          dark: '#060606',
+          DEFAULT: '#060606',
+          light: '#F0F0F0',
+        },
+        'bg-primary': {
+          dark: '#111111',
+          DEFAULT: '#111111',
+          light: '#E5E5E5',
+        },
+        'bg-secondary': {
+          dark: '#202020',
+          DEFAULT: '#202020',
+          light: '#D6D6D6',
+        },
+        
+        'fg-primary': {
+          dark: '#E0E0E0',
+          DEFAULT: '#E0E0E0',
+          light: '#1A1A1A',
+        },
+        'fg-secondary': {
+          dark: '#898989',
+          DEFAULT: '#898989',
+          light: '#6E6E6E',
+        },
+        'fg-tertiary': {
+          dark: '#323232',
+          DEFAULT: '#323232',
+          light: '#C2C2C2',
+        },
 
         'spotify-accent': '#1ED760',
       },
