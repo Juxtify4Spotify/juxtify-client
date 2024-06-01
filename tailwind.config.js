21568af0590b6626a5ae8156defacd20deb2e7e9
出口レイ <richard@kanshen.click>
2024-06-01 19:25:24 +0200

    Expanded Tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,23 @@ export default {
     extend: {
       fontFamily: {
         'satoshi': ['"Satoshi"', 'sans-serif'] 
-      }
+      },
+      colors: {
+        'bg-dark-main': '#060606',
+        'bg-dark-primary': '#111111',
+        'bg-dark-secondary': '#202020',
+
+        'bg-light': '#F0F0F0',
+
+        'fg-dark-primary': '#E0E0E0',
+        'fg-dark-secondary': '#898989',
+        'fg-dark-tertiary': '#323232',
+
+        'fg-light-primary': '#1A1A1A',
+        'fg-light-secondary': '#6E6E6E',
+
+        'spotify-accent': '#1ED760',
+      },
     },
   },
   plugins: [],
