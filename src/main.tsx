53ccba90a06e82784546affbba8a14a457fcf4e6
BostonRohan <bostonrohan@gmail.com>
2024-06-01 17:09:05 -0400

    feat: react-rotuer

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
+import "./assets/styles/App.css";
+import "./assets/styles/Fonts.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
---
+++ src/routes/root.tsx
@@ -0,0 +1,11 @@
+import Menu from "../components/Menu";
+
+function Root() {
+  return (
+    <div>
+      <Menu />
+    </div>
+  );
+}
+
+export default Root;
