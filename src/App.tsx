396d0193ac734a54ae01bed0a26bc1767266740d
KennLDN <matt@kenn.pro>
2024-06-01 20:22:05 +0100

    Reworked for light/dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./assets/styles/Fonts.css";
 
 function App() {
   return (
-  <div className="text-white text-[40px] flex h-screen w-screen items-center justify-center">
+  <div className="text-fg-primary dark:text-fg-primary-dark text-[40px] flex h-screen w-screen items-center justify-center">
     Juxtify
   </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: 'selector',
   theme: {
     extend: {
       fontFamily: {
@@ -11,34 +12,28 @@ export default {
       colors: {
         'bg-main': {
           dark: '#060606',
-          DEFAULT: '#060606',
-          light: '#F0F0F0',
+          DEFAULT: '#F0F0F0',
         },
         'bg-primary': {
           dark: '#111111',
-          DEFAULT: '#111111',
-          light: '#E5E5E5',
+          DEFAULT: '#E5E5E5',
         },
         'bg-secondary': {
           dark: '#202020',
-          DEFAULT: '#202020',
-          light: '#D6D6D6',
+          DEFAULT: '#D6D6D6',
         },
         
         'fg-primary': {
           dark: '#E0E0E0',
-          DEFAULT: '#E0E0E0',
-          light: '#1A1A1A',
+          DEFAULT: '#1A1A1A',
         },
         'fg-secondary': {
           dark: '#898989',
-          DEFAULT: '#898989',
-          light: '#6E6E6E',
+          DEFAULT: '#6E6E6E',
         },
         'fg-tertiary': {
           dark: '#323232',
-          DEFAULT: '#323232',
-          light: '#C2C2C2',
+          DEFAULT: '#C2C2C2',
         },
 
         'spotify-accent': '#1ED760',
