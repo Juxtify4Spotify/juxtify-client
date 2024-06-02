d0a117cb19cba1888ceea58596d9f6ec0b53ad4e
OK1ez <87883296+OK1ez@users.noreply.github.com>
2024-06-02 15:18:51 +0200

    fix: correct colors from figma

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,6 +1,6 @@
 export default function Sidebar() {
   return (
-    <aside className="flex-shrink-0 h-full bg-secondary rounded-xl w-72">
+    <aside className="flex-shrink-0 h-full bg-primary rounded-xl w-72">
 
     </aside>
   );
