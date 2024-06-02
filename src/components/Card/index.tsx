0fc06c112cb35f1bcb59ec2f8f45cc1303d36eae
OK1ez <87883296+OK1ez@users.noreply.github.com>
2024-06-02 17:16:10 +0200

    feat: card variants
    adds artist, folder and album cards variants

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,4 +1,6 @@
+
 interface CardProps {
+  variant: "folder" | "playlist" | "dailyMix" | "album" | "artist" | string;
   title: string;
   tracksCount?: number;
   description: string;
@@ -7,33 +9,49 @@ interface CardProps {
 }
 
 export default function Card({
+  variant,
   title,
   tracksCount,
   primaryColor = "",
   description,
   imageUrl,
 }: CardProps) {
+ 
   return (
     <article className="max-w-[170px] shrink-0">
       <div className="max-h-[181px] w-full h-full gap-[2px] flex flex-col mb-[8px]">
+      { (variant === "folder" || variant === "album") && (
         <div
           style={{ backgroundColor: primaryColor }}
-          className="w-full h-[6px] mx-auto opacity-15 bg-primary-foreground max-w-[133px] rounded-md rounded-b-none"
+          className={`${
+            variant === "album" ? "w-full" : "w-20"
+          } h-[9px] opacity-30 bg-primary-foreground max-w-[154px] ml-2 rounded-md rounded-b-none`}
         />
-        <div
-          style={{ backgroundColor: primaryColor }}
-          className="w-full h-[9px] opacity-30 bg-primary-foreground max-w-[154px] mx-auto rounded-md rounded-b-none"
-        />
+      )}
+
+        {variant !== "artist" && variant !== "folder"  && variant !== "album" && (
+          <>
+            <div
+              style={{ backgroundColor: primaryColor }}
+              className="w-full h-[6px] mx-auto opacity-15 bg-primary-foreground max-w-[133px] rounded-md rounded-b-none"
+            />
+            <div
+              style={{ backgroundColor: primaryColor }}
+              className="w-full h-[9px] opacity-30 bg-primary-foreground max-w-[154px] mx-auto rounded-md rounded-b-none"
+            />
+          </>
+        )}
         <img
-          className="w-full h-[170px] mx-auto rounded-md"
+          className={`w-full h-[170px] mx-auto rounded-md ${variant === "artist" ? "rounded-full" : ""}`}
           src={imageUrl}
+          alt={title}
         />
       </div>
-      <div className="flex justify-between">
-        <div className="pb-2 truncate text-primary-foreground-dark whitespace-nowrap">
+      <div className={`flex justify-between ${variant === "artist" ? "justify-center" : ""}`}>
+        <div className={`pb-2 truncate text-primary-foreground-dark whitespace-nowrap ${variant === "artist" ? "text-center w-full" : ""}`}>
           {title}
         </div>
-        {tracksCount && (
+        {tracksCount && variant !== "artist" && (
           <div className="font-light" style={{ color: primaryColor }}>
             {tracksCount}
           </div>
