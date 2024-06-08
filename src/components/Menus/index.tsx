053a1520ce50fb43c2efea89b2a45553e83a0c70
kenn <matt@kenn.pro>
2024-06-08 11:41:31 +0000

    Fixed crazy icon implementation

--- src/components/Menus/index.tsx
+++ src/components/Menus/index.tsx
@@ -1,10 +1,10 @@
-import Icon from '../Icon/index.tsx'
+import Discover from "../../assets/icons/discover.svg?react";
 
 function Menu() {
     return (
         <nav className="font-medium flex w-full h-20 p-2">
             <div className="flex text-foreground h-full w-72 p-4 bg-secondary gap-3 items-center rounded-lg">
-                <Icon name="discover" filled="yes" color="foreground" />
+                <Discover className="text-foreground" />
                 <p>Discover</p>
             </div>
         </nav>
