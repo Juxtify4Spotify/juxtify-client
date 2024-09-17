6cae2cb4459e9f035265b869b6e23076438195b8
Aaron <aaronvendedor@gmail.com>
2024-09-17 12:27:38 -0400

    Refactored sidebar items

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,62 +1,7 @@
-import Like from '../../assets/icons/like.svg?react';
-import Save from '../../assets/icons/save.svg?react';
-import Album from '../../assets/icons/album.svg?react';
-import Folder from '../../assets/icons/folder.svg?react';
-import Podcast from '../../assets/icons/podcast.svg?react';
-import Audiobook from '../../assets/icons/audiobook.svg?react';
-import Artist from '../../assets/icons/artist.svg?react';
+import { MenuItems, PlaylistItems } from './consts';
 import MenuItem from './MenuItem';
 import PlaylistItem from './SidebarItem/PlaylistItem';
 import PinItem from './SidebarItem/PinItem';
-
-const MenuItems = [
-  {
-    title: 'Liked songs',
-    href: '/',
-    icon: <Like />,
-  },
-  {
-    title: 'Saves',
-    href: '/',
-    icon: <Save />,
-  },
-  {
-    title: 'Albums',
-    href: '/',
-    icon: <Album />,
-  },
-  {
-    title: 'Folders',
-    href: '/',
-    icon: <Folder />,
-  },
-  {
-    title: 'Podcasts',
-    href: '/',
-    icon: <Podcast />,
-  },
-  {
-    title: 'Audiobooks',
-    href: '/',
-    icon: <Audiobook />,
-  },
-  {
-    title: 'Artists',
-    href: '/',
-    icon: <Artist />,
-  },
-];
-
-const PlaylistItems = [
-  {
-    title: 'Chill n stuff',
-    href: '/',
-  },
-  {
-    title: 'Rock n roll',
-    href: '/',
-  },
-];
 
 interface SidebarProps {
   children: React.ReactNode;
